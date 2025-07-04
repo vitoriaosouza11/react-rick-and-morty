d570ff362a18442a95f63d9a467eef30f43e22da
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2025-07-03 22:25:57 -0300

    card

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ function App() {
   
 
   return (
-    <>
-      <h1>Ola mundo</h1>
-    </>
+  <>
+    <div className='card'>
+       <h1 className='card-title'>Rick Sanchez</h1>
+
+       <span className='tag'>Status: Vivo</span>
+       <span className='tag'>Espécie: Humana</span>
+       <span className='tag'>Origem: Terra C-137</span>
+
+      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" className='card-image' />
+    </div>
+  </>
   )
 }
 
